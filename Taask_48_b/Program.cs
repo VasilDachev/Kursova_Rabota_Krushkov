c4c1b9e04a544cf4c658abe72417cccde65f28b9
VasilDachev <vasildachevnew@gmail.com>
2018-11-26 18:33:34 +0200

    48b

---
+++ Taask_48_b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taask_48_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Зад48. Запишете като фрагмент от програма следната функция:
+             б) y=x+2 за x<=0, y=x-2 за 0<x<1 и y=3x за x>=1  
+             */
+            Console.WriteLine("Enter X:");
+            double x = double.Parse(Console.ReadLine());
+            double y;
+            if (x <= 0)
+            {
+                y = x + 2;
+            }
+            else
+            {
+                if (x < 1 && x > 0)
+                {
+                    y = x - 2;
+                }
+                else
+                {
+                    y = 3 * x;
+                }
+            }
+            Console.WriteLine("The result is {0}", y);
+        }
+    }
+}
