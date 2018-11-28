ddeb03858c82eaebdd50a28832f3d124929db90f
VasilDachev <vasildachevnew@gmail.com>
2018-11-28 12:15:03 +0200

    2 more

---
+++ Task_359/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_359
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 359зад. Съставете метод, който в даден знаков низ S търси подниз X, който 
+             е предшестван или последван от поднизове A и B.*/
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Въведете низ S:");
+            string S = Console.ReadLine();
+            DoMyCheck(S);
+            
+        }
+        static string DoMyCheck(string S)
+        {
+            
+            Console.WriteLine("Въведете низ Х");
+            string X = Console.ReadLine();
+            int Choice;
+            Console.WriteLine("За А и В предшестащи, моля натиснете 1");
+            Console.WriteLine("За А и В последващи, моля натиснете 2");
+            Choice = int.Parse(Console.ReadLine());
+            if (Choice == 1)
+            {
+                Console.WriteLine("Въведете низ A");
+                string A = Console.ReadLine();
+
+                Console.WriteLine("Въведете низ B");
+                string B = Console.ReadLine();
+
+                string Combined1 = A + B + X;
+                Console.WriteLine("Предшестван от низове А и В : {0}", Combined1);
+
+                if (S.Contains(X)&&Combined1.Contains(X))
+                {
+                    Console.WriteLine("В низът {0} има подниз {1}, предшестван от {2}", S, X, A+B);
+                }
+                else
+                {
+                    Console.WriteLine("Не съдържа низ Х");
+                }
+
+
+            }
+            else
+            {
+                if(Choice == 2)
+                {
+                    Console.WriteLine("Въведете низ A");
+                    string A = Console.ReadLine();
+
+                    Console.WriteLine("Въведете низ B");
+                    string B = Console.ReadLine();
+
+                    string Combined2 = X + A + B;
+                    Console.WriteLine("Последван от низове А и В : {0}", Combined2);
+                    if (S.Contains(X) && Combined2.Contains(X))
+                    {
+                        Console.WriteLine("В низът {0} има подниз {1}, последван от {2}", S, X, A + B);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не съдържа низ Х");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("1 or 2 not {0}", Choice);
+                }
+            }
+
+            return X;
+            
+        }
+        
+    }
+}
---
+++ Task_transform/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_transform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2087 в 2, 8 и 16 -тична бройни системи.
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int choice;
+            Console.WriteLine("Изберете опции 1, 2, 3");
+            Console.WriteLine("1-към двоична");
+            Console.WriteLine("2-към осмична");
+            Console.WriteLine("3-към шестнадесетична");
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                int number = int.Parse(Console.ReadLine());
+                string Binary = Convert.ToString(number, 2);
+                Console.WriteLine("Числото е {0}", Binary);
+            }
+            else
+            {
+                if (choice == 2)
+                {
+                    int number2 = int.Parse(Console.ReadLine());
+                    string Octa = Convert.ToString(number2, 8);
+                    Console.WriteLine("Числото е {0}", Octa);
+                }
+                else
+                {
+                    if (choice == 3)
+                    {
+                        int number3 = int.Parse(Console.ReadLine());
+                        string Hexa = Convert.ToString(number3, 16);
+                        Console.WriteLine("Числото е {0}", Hexa);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Въведи число между 1 и 3 !");
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
